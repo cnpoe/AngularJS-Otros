132f2d87cdc90528ea085d224c077aea580e84f5
cnpoe <cnpoek@gmail.com>
2017-01-04 23:52:53 -0600

    verbo post

--- http/controller.js
+++ http/controller.js
@@ -6,6 +6,7 @@ var app = angular.module("MyFirstApp",[]);
 
 app.controller("FirstController", function ($scope, $http) {
 	$scope.posts = [];
+	$scope.newPost = {};
 	$http.get("https://jsonplaceholder.typicode.com/posts")
 		.success(function(data){
 			console.log(data);
@@ -14,4 +15,18 @@ app.controller("FirstController", function ($scope, $http) {
 		.error(function(err){
 			console.log(err);
 		});
+	$scope.addPost = function(){
+		$http.post("https://jsonplaceholder.typicode.com/posts", {
+				title: $scope.newPost.title,
+				body: $scope.newPost.body,
+				userId: 1,
+			})
+			.success(function(data, status, headers, config){
+				$scope.posts.push($scope.newPost);
+				$scope.newPost = {};
+			})
+			.error(function(error, status, headers, config){
+				console.log(error);
+			});
+	}
 });
